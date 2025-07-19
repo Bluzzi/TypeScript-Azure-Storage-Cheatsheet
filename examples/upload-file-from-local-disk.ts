ec3b3b741dbaedf4e21900be2ee5f2d36d09b477
Bluzzi <camilledugas.pro@gmail.com>
2025-07-20 00:51:47 +0200

    feat(upload-file): add example for uploading a file from local disk to blob storage

---
+++ examples/upload-file-from-local-disk.ts
@@ -0,0 +1,15 @@
+import { containerClient } from "./client-initialization";
+import { readFileSync, writeFileSync, createReadStream } from "node:fs";
+import { join } from "node:path";
+
+const blockBlobClient = containerClient.getBlockBlobClient("blob-name.txt");
+
+// Create local txt file with random UUID as content:
+const localFile = join(import.meta.dirname, "local-data.txt");
+writeFileSync(localFile, crypto.randomUUID(), "utf-8");
+
+// Example without streaming:
+await blockBlobClient.uploadData(readFileSync(localFile));
+
+// Example with streaming:
+await blockBlobClient.uploadStream(createReadStream(localFile));
